882ebbaa122add35c85dd50d76a9f4a07c06b13c
Jonathan Phair <jmphair@gmail.com>
2023-03-08 14:34:03 -0500

    add: dashboard counts and links

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,11 @@
 class Admin::DashboardController < ApplicationController
+
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
+
   def show
+    @products_count = Product.count 
+    @categories_count = Category.count
+    @orders_count = Order.count 
   end
+
 end
