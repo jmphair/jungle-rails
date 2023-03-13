c7a7b38638b3432e6e4fc5b2c0bc7f511bc5ca74
Jonathan Phair <jmphair@gmail.com>
2023-03-13 19:20:16 -0400

    add: test to visit product page

--- cypress/integration/product_details.spec.js
+++ cypress/integration/product_details.spec.js
@@ -8,4 +8,8 @@ describe('Home Page', () => {
   it("Shows 2 products on the page.", () => {
     cy.get(".products article").should("have.length", 2);
   });
+  it("Gets the product page when a product is clicked.", () => {
+    cy.get("[alt='Giant Tea']").click();
+    cy.get(".product-detail").should("be.visible");
+  });
 });
